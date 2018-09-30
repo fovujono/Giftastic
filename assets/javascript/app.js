645887f60b69a6a025251b61396e1c0bcdeda7b5
smoot <smoothtacomann@gmail.com>
2018-09-29 23:07:41 -0400

    some psuedocode added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,31 @@
-123
+$(document).ready(function () {
+// initial array of animals
+
+var animalArray = ["Pig","Monkey", "Dog"];
+
+
+//onlick function
+$("button").on("click",function(){
+//storing animal data
+
+
+})
+
+//giphy API URl
+
+var queryUrl = //insert api 
+
+//display animals to html
+
+function displayAnimal (){
+
+}
+
+//AJAX call for buttons being clicked
+
+//
+
+
+
+
+});
